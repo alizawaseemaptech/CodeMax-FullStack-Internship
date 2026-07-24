ee352aff15b530bbd25399524e3437456f1978df
alizawaseemaptech <alizawaseemaptech@gmail.com>
2026-07-24 08:47:01 -0700

    Completed Day 5 Express.js GET and POST API

--- Day-02-HTML/server.js
+++ Day-02-HTML/server.js
@@ -5,10 +5,21 @@ const PORT = 3000;
 
 app.use(express.json());
 
+// Home Route (GET)
 app.get("/", (req, res) => {
     res.send("Welcome to Simple Blog Management System API");
 });
 
+// GET All Blogs Route
+app.get("/blogs", (req, res) => {
+    res.json({
+        success: true,
+        message: "Blogs fetched successfully",
+        blogs: []
+    });
+});
+
+// POST Add Blog Route
 app.post("/add-blog", (req, res) => {
     const { title, author, content } = req.body;
 
@@ -23,6 +34,7 @@ app.post("/add-blog", (req, res) => {
     });
 });
 
+// Start Server
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
